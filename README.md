4620446ccecc61fa4ac59ccde93f242610cdd332
Andy Deng <andy.z.deng@gmail.com>
2025-12-29 21:43:10 +1300

    v 1.0.5 pass parameters with env variables

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ npm install --save-dev bye-esm-ext
 
 # Usage
 ```ts
-// If you prefer esm module instead of commonjs, but also this style:
+// If you prefer not only esm module instead of commonjs, but also:
 
 import { foo, bar } from './my-module'
 
@@ -19,7 +19,20 @@ import { foo, bar } from './my-module'
 // import { foo, bar } from './my-module.js'
 // import { foo, bar } from './my-module.ts'
 ```
+
 Use this as a node.js loader parameter:
+```sh
+node --import bye-esm-ext your-app.js
 ```
-node --loader bye-esm-ext your-app.js
+
+## Special case
+In certain special cases, the imports in your .js files within the output directory may resolve filenames back to the source directory.
+
+For example, when debugging a module, you might use a file: link in your dependencies to point a package to a .ts file in your source directory. bye-esm-ext can help you resolve it to the corresponding compiled .js file in the output directory.
+
+To do this, you need to provide two parameters from your tsconfig.json: `rootDir` and `outDir`, link them with `:` and assign it to an environment variable BYE_ESM_EXT_MAP_ROOT_TO.
+
+Pass the variable as follows:
+```sh
+BYE_ESM_EXT_MAP_ROOT_TO=./src:./dist node --import=bye-esm-ext your-app.js
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
-import type { ResolveHook } from 'node:module'
+import { register, type ResolveHook } from 'node:module'
 import { existsSync, statSync } from 'node:fs'
 import { dirname, extname, resolve as resolvePath } from 'node:path'
 
+register(new URL(import.meta.url))
+
 const extensions = ['.js', '.mjs', '.json', '.jsx', '.d.ts', '.ts', '.tsx']
+const mapTo = process.env['BYE_ESM_EXT_MAP_ROOT_TO']
+let rootDir = '', outDir = ''
+if (mapTo) {
+    const [root, out] = mapTo.split(':')
+    if (root && out) {
+        const cwd = process.cwd()
+        rootDir = root[0] === '/' ? resolvePath(root) : resolvePath(cwd, root)
+        outDir = out[0] === '/' ? resolvePath(out) : resolvePath(cwd, out)
+    }
+}
 
 export const resolve: ResolveHook = (specifier, context, nextResolve) => {
     const fileExt = extname(specifier)
     if ((!fileExt || !extensions.includes(fileExt)) && context.parentURL?.startsWith('file://')) {
         const parentPath = context.parentURL.substring(7)
-        const dirName = dirname(parentPath)
+        let dirName = dirname(parentPath)
+        if (rootDir && dirName.startsWith(rootDir)) {
+            dirName = dirName.replace(rootDir, outDir)
+        }
         let fullPath = resolvePath(dirName, specifier)
         if (!existsSync(fullPath)) {
             for (const ext of extensions) {
